26d887a789a707c91f0467e049d9e1f0759f9d65
Sachin Jaiswal <sj586997@gmail.com>
2025-11-04 17:36:29 +0530

    added dotenv

--- server/config/db.js
+++ server/config/db.js
@@ -9,7 +9,6 @@ const connectDB = async () => {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-        console.log(process.env.MONGODB_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
